2e11602a5e8789e2b365544c731fd103ff645c1c
Brian Kuzma <hello@briankuzma.com>
2017-09-13 16:44:08 -0400

    add some more content to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,35 @@
-import React from "react"
+import React from 'react';
+import styled, { injectGlobal } from 'styled-components';
 
-export default () => <div>Hello world!</div>
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const LandingTile = () => {
+
+  const Wrapper = styled.div`
+    align-items: center;
+    background-color: papayawhip;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    padding: 20px;
+  `;
+
+  const Header = styled.h2`
+    margin: 0;
+  `;
+
+  return (
+    <Wrapper>
+      <Header>Brian Kuzma is a Front-End Engineer based in New York.</Header>
+    </Wrapper>
+  );
+};
+
+export default () => (
+  <LandingTile />
+);
