eb03f4a4773a5e1723f28d41ea5006cd8384fc0e
Brian Kuzma <hello@briankuzma.com>
2017-09-14 17:36:14 -0400

    add dummy json for projects and render them from Work component

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Project from '../components/Project';
+
+const Wrapper = styled.div`
+  background-color: lavenderblush;
+  overflow: auto;
+  padding: 0 20px;
+`;
+
+const projects = [
+  {
+    agency: 'R/GA',
+    description: 'blah blah blah',
+    images: [],
+    link: null,
+    title: 'Samsung Rihanna'
+  },
+  {
+    agency: 'Huge',
+    description: 'blah blah blah',
+    images: [],
+    link: null,
+    title: 'Hugeinc.com'
+  }
+];
+
 export default () => (
-  <div id="work">
-    Here's all my work!
-  </div>
+  <Wrapper id="work">
+    <h3>Selected Work</h3>
+    <div>
+      {projects.map((project, index) => (
+        <Project {...project} key={index} />
+      ))}
+    </div>
+  </Wrapper>
 );
