55d0f66b6cc153dd87499a6ea784bc5ed38c78d6
Brian Kuzma <hello@briankuzma.com>
2017-09-15 18:28:13 -0400

    update projects copy and add year

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,7 +8,7 @@ import kswiss from '../images/kswiss-1.png';
 export default [
   {
     agency: 'R/GA',
-    description: 'I developed a motion-activated mobile audio player app in Angular using the device motion JavaScript API. The app promoted Samsung’s fitness products and would play an exclusive music mix by Diplo only when a certain threshold of device movement was detected to encourage dancing/running/walking/etc. Done while at R/GA.',
+    description: 'I developed a motion-activated mobile audio player app in Angular using the device motion JavaScript API. The app promoted Samsung’s fitness products and would play an exclusive music mix by Diplo only when a certain threshold of device movement was detected to encourage dancing, running, walking, and so on.',
     images: [
       {
         src: diplo,
@@ -16,10 +16,11 @@ export default [
     ],
     link: null,
     title: 'Move With Galaxy',
+    year: 2016,
   },
   {
     agency: 'R/GA',
-    description: 'I worked on a team developing an immersive 3D mobile app in Angular for Rihanna in partnership with Samsung to promote her 2016 album release. I was responsible for overall app architecture and routing as well as writing and styling many of the app’s Angular components. Done while at R/GA.',
+    description: 'I worked on a team developing an immersive 3D mobile app in Angular for Rihanna in partnership with Samsung to promote her 2016 album release. I was responsible for overall app architecture and routing as well as writing and styling many of the app’s Angular components.',
     images: [
       {
         src: antidiary,
@@ -27,25 +28,18 @@ export default [
     ],
     link: null,
     title: 'Rihanna Antidiary',
-  },
-  {
-    description: 'I did all front end development for a previous version of K-Swiss’s website.',
-    images: [
-      {
-        src: kswiss,
-      },
-    ],
-    title: 'K-Swiss',
+    year: 2015,
   },
   {
     agency: 'NoFavorite',
-    description: 'I did all front end development for the Fanmail e-commerce site. Fully responsive. Done through NoFavorite.',
+    description: 'I did all front end development for the Fanmail e-commerce site. Fully responsive.',
     images: [
       {
         src: fanmail,
       },
     ],
     title: 'Fanmail',
+    year: 2013,
   },
   {
     description: 'I wrote all grid touch interaction for Destroyer, an iPhone app by The Strange Agency that lets you rearrange and manipulate sound loops.',
@@ -55,6 +49,17 @@ export default [
       },
     ],
     title: 'Destroyer',
+    year: 2012,
+  },
+  {
+    description: 'I did all front end development for a previous version of K-Swiss’s website.',
+    images: [
+      {
+        src: kswiss,
+      },
+    ],
+    title: 'K-Swiss',
+    year: 2011,
   },
   {
     description: 'I did all development for Bite Me, an iPad game available for free in the iTunes app store. Written in Objective-C.',
@@ -64,5 +69,6 @@ export default [
       },
     ],
     title: 'Bite Me',
+    year: 2011,
   },
 ];
