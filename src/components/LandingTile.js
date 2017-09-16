1a8af1fb72908f309d4144c580c323bf52287128
Brian Kuzma <hello@briankuzma.com>
2017-09-16 11:45:37 -0400

    update LandingTile to use unicode icons, remove Chevron

--- src/components/LandingTile.js
+++ src/components/LandingTile.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Chevron from '../components/Chevron';
-
 export default ({ onClickScroll }) => {
+
+  const Arrow = styled.div`
+    bottom: 40px;
+    cursor: pointer;
+    fill: #000;
+    font-size: ${30 / 16}rem;
+    left: 50%;
+    position: absolute;
+    transform: translateX(-50%) rotate(90deg);
+  `;
 
   const Wrapper = styled.div`
     align-items: center;
     background-color: papayawhip;
     display: flex;
+    flex-direction: column;
     height: 100vh;
     justify-content: center;
     padding: 20px;
@@ -18,21 +27,13 @@ export default ({ onClickScroll }) => {
     margin: 0;
   `;
 
-  const ChevronContainer = styled.div`
-    bottom: 40px;
-    cursor: pointer;
-    fill: #000;
-    left: 50%;
-    position: absolute;
-    transform: translateX(-50%);
-  `;
-
   return (
     <Wrapper>
-      <Header>Brian Kuzma is a Front-End Engineer based in New York.</Header>
-      <ChevronContainer onClick={onClickScroll}>
-        <Chevron />
-      </ChevronContainer>
+      <h1>✌&#xFE0E; ☺&#xFE0E;</h1>
+      <Header>I'm a Front-End Web Engineer based in New York.</Header>
+      <Arrow onClick={onClickScroll}>
+        ➱&#xFE0E;
+      </Arrow>
     </Wrapper>
   );
 };
