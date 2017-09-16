e5056a91650c996c8056ee5ccfa1c3c52d64b196
Brian Kuzma <hello@briankuzma.com>
2017-09-16 12:05:21 -0400

    add some copy and contact links to Landing Tile

--- src/components/LandingTile.js
+++ src/components/LandingTile.js
@@ -17,8 +17,8 @@ export default ({ onClickScroll }) => {
     margin-top: 0;
   `;
 
-  const Subheader = styled.h2`
-    margin: 0;
+  const Subheader = styled.h3`
+    margin-top: 0;
   `;
 
   const Wrapper = styled.div`
@@ -29,12 +29,44 @@ export default ({ onClickScroll }) => {
     height: 100vh;
     justify-content: center;
     padding: 20px;
+    text-align: center;
   `;
+
+  const emailLink =
+    <a
+      href="mailto:hello@briankuzma.com"
+      target="_blank">
+      email
+    </a>;
+
+  const linkedInLink =
+    <a
+      href="https://www.linkedin.com/in/briankuzma/"
+      target="_blank">
+      LinkedIn
+    </a>;
+
+  const gitHubLink =
+    <a
+      href="https://github.com/bkuzma"
+      target="_blank">
+      GitHub
+    </a>;
 
   return (
     <Wrapper>
       <Header>✌&#xFE0E; ☺&#xFE0E;</Header>
-      <Subheader>I'm a Front-End Web Engineer based in New York.</Subheader>
+      <Subheader>
+        I'm a Front-End Web Engineer based in New York.
+      </Subheader>
+      <p>
+        I build web applications using technologies like
+        React, Redux, ES7, Webpack, and more.
+      </p>
+      <p>
+        Find me on {linkedInLink}, {gitHubLink},
+        or {emailLink}.
+      </p>
       <Arrow onClick={onClickScroll}>
         ➱&#xFE0E;
       </Arrow>
