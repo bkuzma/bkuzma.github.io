5a8a147de39830ff5c7e7b900411c78d260bd473
Brian Kuzma <hello@briankuzma.com>
2017-09-17 09:54:47 -0400

    move rihanna project to the top

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,18 +7,6 @@ import kswiss from '../images/kswiss-1.png';
 export default [
   {
     agency: 'R/GA',
-    description: 'I developed a motion-activated mobile audio player app in Angular using the device motion JavaScript API. The app promoted Samsung’s fitness products and would play an exclusive music mix by Diplo only when a certain threshold of device movement was detected to encourage dancing, running, walking, and so on.',
-    images: [
-      {
-        src: diplo,
-      },
-    ],
-    link: null,
-    title: 'Move With Galaxy',
-    year: 2016,
-  },
-  {
-    agency: 'R/GA',
     description: 'I worked on a team developing an immersive 3D mobile app in Angular for Rihanna in partnership with Samsung to promote her 2016 album release. I was responsible for overall app architecture and routing as well as writing and styling many of the app’s Angular components.',
     images: [
       {
@@ -28,6 +16,18 @@ export default [
     link: null,
     title: 'Rihanna Antidiary',
     year: 2015,
+  },
+  {
+    agency: 'R/GA',
+    description: 'I developed a motion-activated mobile audio player app in Angular using the device motion JavaScript API. The app promoted Samsung’s fitness products and would play an exclusive music mix by Diplo only when a certain threshold of device movement was detected to encourage dancing, running, walking, and so on.',
+    images: [
+      {
+        src: diplo,
+      },
+    ],
+    link: null,
+    title: 'Move With Galaxy',
+    year: 2016,
   },
   {
     description: 'Destroyer is an iPhone app from The Strange Agency that lets you rearrange and manipulate sound loops. I built the interactive grid UI in Objective-C, which interfaced with the app\'s audio engine and allowed the user to rearrange and toggle tiles, thereby changing the structure of a sound loop in real-time',
