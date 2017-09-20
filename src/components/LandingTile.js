2f33c224d138eb79917773b1ed1fb7694e9f2a2b
Brian Kuzma <hello@briankuzma.com>
2017-09-20 09:25:19 -0400

    convert LandingTile to React component and add hover animation to hand icon

--- src/components/LandingTile.js
+++ src/components/LandingTile.js
@@ -5,99 +5,149 @@ import Container from './Container';
 import { media } from '../utils/media-queries';
 import contact from '../data/contact';
 
-export default ({ onClickScroll }) => {
-
-  const downUpAnimation = keyframes`
-    0% {
-      bottom: 10px;
-    }
-    50% {
-      bottom: 0;
-    }
-    100% {
-      bottom: 10px;
-    }
-  `;
-
-  const Arrow = styled.div`
-    animation: ${downUpAnimation} 0.6s 5s 4;
+const downUpAnimation = keyframes`
+  0% {
     bottom: 10px;
-    cursor: pointer;
-    font-size: ${30 / 16}rem;
-    left: 50%;
-    position: absolute;
-    transform: translateX(-50%) rotate(90deg);
-  `;
+  }
+  50% {
+    bottom: 0;
+  }
+  100% {
+    bottom: 10px;
+  }
+`;
 
-  const Header = styled.h1`
-    margin-top: 0;
-  `;
+const Arrow = styled.div`
+  animation: ${downUpAnimation} 0.6s 5s 4;
+  bottom: 10px;
+  cursor: pointer;
+  font-size: ${30 / 16}rem;
+  left: 50%;
+  position: absolute;
+  transform: translateX(-50%) rotate(90deg);
+`;
 
-  const Subheader = styled.h3`
-    margin-top: 0;
-  `;
+const Hand = styled.span`
+  display: inline-block;
+  transform: rotate(0deg);
+  transition: transform 0.3s;
+  ${props => props.isHovered &&
+    'transform: rotate(-40deg);'
+  };
+`;
 
-  const Wrapper = Container.extend`
-    align-items: center;
-    background-color: lightsteelblue;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    justify-content: center;
-    text-align: center;
-    ${media.tablet`
-      left: 0;
-      position: fixed;
-      right: 0;
-      top: 0;
-    `}
-  `;
+const Header = styled.h1`
+  margin-top: 0;
+`;
 
-  const {
-    email,
-    gitHub,
-    linkedIn,
-  } = contact;
+const Subheader = styled.h3`
+  margin-top: 0;
+`;
 
-  const emailLink =
-    <a
-      href={email.url}
-      target="_blank">
-      {email.label}
-    </a>;
+const Wrapper = Container.extend`
+  align-items: center;
+  background-color: lightsteelblue;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  text-align: center;
+  ${media.tablet`
+    left: 0;
+    position: fixed;
+    right: 0;
+    top: 0;
+  `}
+`;
 
-  const linkedInLink =
-    <a
-      href={linkedIn.url}
-      target="_blank">
-      {linkedIn.label}
-    </a>;
+export default class LandingTile extends React.Component {
 
-  const gitHubLink =
-    <a
-      href={gitHub.url}
-      target="_blank">
-      {gitHub.label}
-    </a>;
+  constructor(props) {
+    super(props);
 
-  return (
-    <Wrapper>
-      <Header>✌&#xFE0E; ☺&#xFE0E;</Header>
-      <Subheader>
-        Hi, I'm a Front-End Web Engineer based in New York.
-      </Subheader>
-      <p>
-        I build web applications using technologies like
-        React, Redux, ES7, Webpack, and more.
-      </p>
-      <p>
-        Find me on {linkedInLink}, {gitHubLink},
-        or {emailLink}, or scroll down to see some recent work.
-      </p>
-      <Arrow onClick={onClickScroll}>
-        ➝&#xFE0E;
-      </Arrow>
-    </Wrapper>
-  );
+    this.state = {
+      isHeaderHovered: false,
+    };
+
+    this.onHeaderMouseOver = this.onHeaderMouseOver.bind(this);
+    this.onHeaderMouseOut = this.onHeaderMouseOut.bind(this);
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ isHeaderHovered: true }), 1000);
+    setTimeout(() => this.setState({ isHeaderHovered: false }), 2200);
+  }
+
+  onHeaderMouseOver() {
+    this.setState({
+      isHeaderHovered: true,
+    });
+  }
+
+  onHeaderMouseOut() {
+    this.setState({
+      isHeaderHovered: false,
+    });
+  }
+
+  render() {
+    const { onClickScroll } = this.props;
+    const { isHeaderHovered } = this.state;
+
+    const {
+      email,
+      gitHub,
+      linkedIn,
+    } = contact;
+
+    const emailLink =
+      <a
+        href={email.url}
+        target="_blank">
+        {email.label}
+      </a>;
+
+    const linkedInLink =
+      <a
+        href={linkedIn.url}
+        target="_blank">
+        {linkedIn.label}
+      </a>;
+
+    const gitHubLink =
+      <a
+        href={gitHub.url}
+        target="_blank">
+        {gitHub.label}
+      </a>;
+
+    return (
+      <Wrapper>
+        <Header
+          onMouseEnter={this.onHeaderMouseOver}
+          onMouseLeave={this.onHeaderMouseOut}
+        >
+          <Hand isHovered={isHeaderHovered}>
+            ✌&#xFE0E;
+          </Hand>
+          ☺&#xFE0E;
+        </Header>
+        <Subheader>
+          Hi, I'm a Front-End Web Engineer based in New York.
+        </Subheader>
+        <p>
+          I build web applications using technologies like
+          React, Redux, ES7, Webpack, and more.
+        </p>
+        <p>
+          Find me on {linkedInLink}, {gitHubLink},
+          or {emailLink}, or scroll down to see some recent work.
+        </p>
+        <Arrow onClick={onClickScroll}>
+          ➝&#xFE0E;
+        </Arrow>
+      </Wrapper>
+    );
+  }
 };
